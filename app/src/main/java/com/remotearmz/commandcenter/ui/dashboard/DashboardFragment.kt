ba5f8846c5f65f67a6a883395a165d84dd5e8f6e
Mr. Ashish Panday <ashishpanday953@gmail.com>
2025-05-29 14:43:20 +0545

    Fix: Added DashboardFragment and resolved layout crash

---
+++ app/src/main/java/com/remotearmz/commandcenter/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,18 @@
+package com.remotearmz.commandcenter.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.remotearmz.commandcenter.R
+
+class DashboardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
